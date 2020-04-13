116012435414d13dbfb45c2855648a2a19eabd20
Tihomir Magdic <tihomir.magdic@gmail.com>
2020-04-13 22:18:47 +0200

    added Qualification

---
+++ src/main/java/com/hck/volmanager/model/Qualification.java
@@ -0,0 +1,60 @@
+package com.hck.volmanager.model;
+
+import javax.persistence.*;
+
+/**
+ * 	id bigserial,
+ * 	name text not null,
+ * 	ordernum int2,
+ */
+
+@Entity
+@Table(name = "qualifications")
+public class Qualification {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "ordernum")
+    private short orderNum;
+
+    @Override
+    public String toString() {
+        return "Qualification{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+    // Setters and getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(short orderNum) {
+        this.orderNum = orderNum;
+    }
+}
