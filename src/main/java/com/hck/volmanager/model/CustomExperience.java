28f881754fdc9a4a7689809130a0b6599cb6f051
Tihomir Magdic <tihomir.magdic@gmail.com>
2020-04-24 12:15:48 +0200

    added CustomExperience, CustomService and CustomSkill

---
+++ src/main/java/com/hck/volmanager/model/CustomExperience.java
@@ -0,0 +1,61 @@
+package com.hck.volmanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * 	volunteerId int8,
+ * 	item int2 not null,
+ * 	name text not null,
+ */
+
+@Entity
+@Table(name = "vcustomexperiences")
+public class CustomExperience implements Serializable {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "volunteerid", referencedColumnName = "id")
+    private Volunteer volunteer;
+
+    @Id
+    @Column(name = "item")
+    private short item;
+
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public String toString() {
+        return "CustomExperience{" +
+                "volunteer=" + volunteer +
+                ", item='" + item + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    // Setters and getters
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    public short getItem() {
+        return item;
+    }
+
+    public void setItem(short item) {
+        this.item = item;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
