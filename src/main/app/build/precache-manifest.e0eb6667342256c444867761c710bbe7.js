fe8692882b231b34f1079bce83b2e241904bff96
Karlo Magdic <karlo.magdic@gmail.com>
2020-04-24 20:16:33 +0200

    Corrected build

---
+++ src/main/app/build/precache-manifest.e0eb6667342256c444867761c710bbe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc7f2930f539c30893b959e18fecc08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46634a15e3d47b5f5659",
+    "url": "/static/css/2.965ebd84.chunk.css"
+  },
+  {
+    "revision": "6bc90c4b8870ba0325f1",
+    "url": "/static/css/main.8a3c6280.chunk.css"
+  },
+  {
+    "revision": "46634a15e3d47b5f5659",
+    "url": "/static/js/2.f0d122cc.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.f0d122cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc90c4b8870ba0325f1",
+    "url": "/static/js/main.2fc814e4.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  },
+  {
+    "revision": "425dea6ec513bd6acd186948ee420e04",
+    "url": "/static/media/volonteri.425dea6e.jpg"
+  }
+]);
