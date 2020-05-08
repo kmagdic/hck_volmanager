62573d2a8b538161228df3d8730031128e5dc7f7
Tihomir Magdic <tihomir.magdic@gmail.com>
2020-05-08 17:06:41 +0200

    added hashing password with BlowFish algorithm

--- src/main/java/com/hck/volmanager/repository/UserRepository.java
+++ src/main/java/com/hck/volmanager/repository/UserRepository.java
@@ -1,12 +1,22 @@
 package com.hck.volmanager.repository;
 
-import com.hck.volmanager.model.Skill;
 import com.hck.volmanager.model.User;
+import com.hck.volmanager.model.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-    User findOneByUsername(String username);
+    @Query(value = "select * from hck.users where enabled and username = :username and crypt(:pass, salt) = pass",
+            nativeQuery = true
+    )
+    User findOneByUsernameAndHashedPass(
+            @Param("username") String username,
+            @Param("pass") String pass
+    );
 }
