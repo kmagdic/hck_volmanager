f577da7899cb154c6fea3e46098eb264f122cc4a
Tihomir Magdic <tihomir.magdic@gmail.com>
2020-09-02 17:42:26 +0200

    removed security to Propjects

--- src/main/java/com/hck/volmanager/controller/ProjectController.java
+++ src/main/java/com/hck/volmanager/controller/ProjectController.java
@@ -2,7 +2,6 @@ package com.hck.volmanager.controller;
 
 import com.hck.volmanager.exception.ForbiddenHttpException;
 import com.hck.volmanager.model.Project;
-import com.hck.volmanager.model.WebUser;
 import com.hck.volmanager.repository.ProjectRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -24,23 +21,13 @@ public class ProjectController {
     private ProjectRepository projectRepository;
 
     @GetMapping("/projects")
-    public List<Project> getAllProjects(HttpSession session) throws ForbiddenHttpException {
-        WebUser webUser = (WebUser) session.getAttribute("webUser");
-        if(webUser == null) {
-            throw new ForbiddenHttpException("Unauthorized operation.");
-        }
-
+    public List<Project> getAllProjects() {
         log.info("Listing all projects ...");
         return projectRepository.findAll();
     }
 
     @GetMapping("/projects/enabled")
-    public List<Project> getAllEnabledProjects(HttpSession session) throws ForbiddenHttpException {
-        WebUser webUser = (WebUser) session.getAttribute("webUser");
-        if(webUser == null) {
-            throw new ForbiddenHttpException("Unauthorized operation.");
-        }
-
+    public List<Project> getAllEnabledProjects() {
         log.info("Listing all projects ...");
         return projectRepository.findAllEnabled();
     }
